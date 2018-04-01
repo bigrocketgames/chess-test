dd1987033dfb0c9ba8676b55d8df69c324422f3e
Nathan Robbert <admin@bigrocketgames.com>
2018-03-31 22:27:53 -0700

    feat:  Updating BoardSpace to display each space on the game board.

--- src/components/boardSpace.js
+++ src/components/boardSpace.js
@@ -3,10 +3,13 @@ import React, { Component } from 'react';
 class BoardSpace extends Component {
 
   render() {
+    const { space } = this.props
     return(
       <div className="space">
-        
+        <p>This is space: {space.id}</p>
       </div>
     )
   }
 }
+
+export default BoardSpace;
