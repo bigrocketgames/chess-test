0a16be3e289f508363aa0d59b80359342209cabf
Nathan Robbert <admin@bigrocketgames.com>
2018-03-31 23:01:58 -0700

    feat:  Adding onClick event to highlight selected cell.

--- src/components/boardSpace.js
+++ src/components/boardSpace.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
 
 class BoardSpace extends Component {
+  constructor(props) {
+    super(props)
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+  
+  handleClick = (e) => {
+    e.preventDefault();
+    e.currentTarget.className = "space selected";
+  }
 
   render() {
     const { space } = this.props
     return(
-      <div className="space">
+      <div className="space" onClick={this.handleClick} >
         <p>This is space: {space.id}</p>
       </div>
     )
