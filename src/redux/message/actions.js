997cdab5be9351a4c25ee3a9f3c611e631667b8c
bigrocketgames <admin@bigrocketgames.com>
2018-04-01 16:07:08 -0700

    feat:  Added action and updated reducer for passing message on click of a board space.

---
+++ src/redux/message/actions.js
@@ -0,0 +1,9 @@
+export const UPDATE_MESSAGE_SUCCESS = "UPDATE_MESSAGE_SUCCESS";
+
+export const updateMessageSuccess = (message) => {
+  return {
+    type: UPDATE_MESSAGE_SUCCESS,
+    message
+  }
+}
+
--- src/redux/message/reducer.js
+++ src/redux/message/reducer.js
@@ -1,6 +1,10 @@
+import { UPDATE_MESSAGE_SUCCESS } from './actions';
+
 export default(state = "", action) => {
   switch(action.type) {
-
+    case UPDATE_MESSAGE_SUCCESS:
+      return action.message;
+      
     default:
       return state;
   }
