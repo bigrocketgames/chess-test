ed918f0e3cc2a8e2b81d795a6acb3adbbb5afdf3
bigrocketgames <admin@bigrocketgames.com>
2018-04-01 16:05:59 -0700

    fix:  Moved click logic to parent and cleaned up code.

--- src/components/boardSpace.js
+++ src/components/boardSpace.js
@@ -1,22 +1,13 @@
 import React, { Component } from 'react';
 
 class BoardSpace extends Component {
-  constructor(props) {
-    super(props)
-
-    this.handleClick = this.handleClick.bind(this);
-  }
   
-  handleClick = (e, space) => {
-    e.preventDefault();
-    console.log("you clicked " + space.props.space.value + " " + space.props.space.space); 
-  }
-
   render() {
-    const { space } = this.props
+    const { space, selected, handleCellClick } = this.props
+
     return(
-      <div className={`space ${space.color}`} onClick={(e) => this.handleClick(e, this)} >
-        <div className={`nameDiv +  ${space.value}`}>
+      <div className={`space ${space.color} ${selected}`} onClick={(e) => handleCellClick(e, space)} >
+        <div className={`nameDiv ${space.value}`}>
           <p className="spaceName">{space.space}</p>
         </div>
       </div>
