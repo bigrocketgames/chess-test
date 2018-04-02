b3e686e9a5f61383c1dacc97d149476b95449190
bigrocketgames <admin@bigrocketgames.com>
2018-04-02 16:49:42 -0700

    feat:  Updated history action and reducer for rewinding history one move at a  time.

--- src/redux/history/actions.js
+++ src/redux/history/actions.js
@@ -1,10 +1,17 @@
 export const ADD_HISTORY_SUCCESS = 'ADD_HISTORY_SUCCESS';
 export const RESET_HISTORY_SUCCESS = 'RESET_HISTORY_SUCCESS';
+export const REWIND_HISTORY_SUCCESS = 'REWIND_HISTORY_SUCCESS';
 
 export const addHistorySuccess = (history) => {
   return({
     type: ADD_HISTORY_SUCCESS,
     history
+  })
+}
+
+export function rewindHistory() {
+  return({
+    type: REWIND_HISTORY_SUCCESS
   })
 }
 
