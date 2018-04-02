a525dc3587f58a820c027ca2a539b36f81c4774a
bigrocketgames <admin@bigrocketgames.com>
2018-04-02 09:26:57 -0700

    feat:  All helpers functioning for checking valid moves for bishop.

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,9 +1,9 @@
-export const canPieceMoveToNewCell = (piece, currentCell, newCell) => {
+export const canPieceMoveToNewCell = (board, piece, currentCell, newCell) => {
   switch(piece) {
     case "Bishop":
       // implement logic to check if valid move
       if (validBishopMove(currentCell, newCell)) {
-        return isBishopBlocked(currentCell, newCell);
+        return !isBishopBlocked(board, currentCell, newCell);
       }
       return false;
 
@@ -24,6 +24,48 @@ const validKnightMove = (currentCell, newCell) => {
   
 }
 
-const isBishopBlocked = (currentCell, newCell) => {
+const isBishopBlocked = (board, currentCell, newCell) => {
   
+  const rowNum = currentCell.row - newCell.row
+  const cellNum = currentCell.cell - newCell.cell
+  let checkCell = null
+
+  if (rowNum > 0) {
+    for(let i = 1; i <= rowNum; i++) {
+      if (cellNum > 0) {
+        checkCell = board.find(function(e) {
+          return ((e.row === currentCell.row - i) && (e.cell === currentCell.cell - i))
+        }) 
+      } else {
+        checkCell = board.find(function(e) {
+          return ((e.row === currentCell.row - i) && (e.cell === currentCell.cell + i))
+        })
+      }
+
+      // check if next cell is currently occupied
+      if (checkCell.value !== "") {
+        return true
+      }
+    }
+  } else {
+    for(let i = -1; i >= rowNum; i--){
+      if (cellNum > 0) {
+        checkCell = board.find(function(e) {
+          return ((e.row === currentCell.row - i) && (e.cell === currentCell.cell + i))
+        }) 
+      } else {
+        checkCell = board.find(function(e) {
+          return ((e.row === currentCell.row - i) && (e.cell === currentCell.cell - i))
+        })
+      }
+
+      // check if next cell is currently occupied
+      if (checkCell.value !== "") {
+        return true
+      }
+    }
+  }
+
+  // if the bishop is not blocked return false
+  return false
 }
