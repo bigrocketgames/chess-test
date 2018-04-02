661523b3ce098c9be0ec9ccae394a9a3e8934907
bigrocketgames <admin@bigrocketgames.com>
2018-04-02 08:13:15 -0700

    chore:  Added class for error animation.

--- src/components/boardSpace.js
+++ src/components/boardSpace.js
@@ -3,10 +3,10 @@ import React, { Component } from 'react';
 class BoardSpace extends Component {
   
   render() {
-    const { space, selected, handleCellClick } = this.props
+    const { space, selected, handleCellClick, flashError } = this.props
 
     return(
-      <div className={`space ${space.color} ${selected}`} onClick={(e) => handleCellClick(e, space)} >
+      <div className={`space ${space.color} ${selected} ${flashError}`} onClick={(e) => handleCellClick(e, space)} >
         <div className={`nameDiv ${space.value}`}>
           <p className="spaceName">{space.space}</p>
         </div>
