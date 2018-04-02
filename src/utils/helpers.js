288874eb306ddaec5bb6e8918ac273da8f93d8f9
bigrocketgames <admin@bigrocketgames.com>
2018-04-02 08:14:41 -0700

    feat:  function validBishopMove is now working.  Working on second method to verify bishop movement.

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -2,7 +2,10 @@ export const canPieceMoveToNewCell = (piece, currentCell, newCell) => {
   switch(piece) {
     case "Bishop":
       // implement logic to check if valid move
-      return true;
+      if (validBishopMove(currentCell, newCell)) {
+        return isBishopBlocked(currentCell, newCell);
+      }
+      return false;
 
     case "Knight":
       // implement logic to check if valid move
@@ -14,9 +17,13 @@ export const canPieceMoveToNewCell = (piece, currentCell, newCell) => {
 }
 
 const validBishopMove = (currentCell, newCell) => {
-
+  return Math.abs(currentCell.row - newCell.row) === Math.abs(currentCell.cell - newCell.cell);
 }
 
 const validKnightMove = (currentCell, newCell) => {
+  
+}
+
+const isBishopBlocked = (currentCell, newCell) => {
   
 }
