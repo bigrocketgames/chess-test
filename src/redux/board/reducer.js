753efaa6be8bf18eff2bd6d98ecc2a6824e69072
bigrocketgames <admin@bigrocketgames.com>
2018-05-06 10:53:15 -0700

    feat: Updating initial board state to have all beginning pieces.

--- src/redux/board/reducer.js
+++ src/redux/board/reducer.js
@@ -1,9 +1,23 @@
 import { MOVE_SUCCESS, RESET_BOARD_SUCCESS, REWIND_BOARD_SUCCESS } from './actions';
 
 const initialState = { board: [
-  {id: 1, row: 1, cell: 1, space: "a8", value: "", color: "light", piece: "", pieceColor: ""}, {id: 2, row: 1, cell: 2, space: "b8", value: "", color: "dark", piece: "", pieceColor: ""}, {id: 3, row: 1, cell: 3, space: "c8", value: "", color: "light", piece: "", pieceColor: ""}, {id: 4, row: 1, cell: 4, space: "d8", value: "", color: "dark", piece: "", pieceColor: ""}, {id: 5, row: 1, cell: 5, space: "e8", value: "", color: "light", piece: "", pieceColor: ""}, {id: 6, row: 1, cell: 6, space: "f8", value: "bishopblack", color: "dark", piece: "Bishop", pieceColor: "Black"}, {id: 7, row: 1, cell: 7, space: "g8", value: "knightblack", color: "light", piece: "Knight", pieceColor: "Black"}, {id: 8, row: 1, cell: 8, space: "h8", value: "", color: "dark", piece: "", pieceColor: ""},
+  {id: 1, row: 1, cell: 1, space: "a8", value: "rookblack", color: "light", piece: "Rook", pieceColor: "Black"}, 
+  {id: 2, row: 1, cell: 2, space: "b8", value: "knightblack", color: "dark", piece: "Knight", pieceColor: "Black"}, 
+  {id: 3, row: 1, cell: 3, space: "c8", value: "bishopblack", color: "light", piece: "Bishop", pieceColor: "Black"}, 
+  {id: 4, row: 1, cell: 4, space: "d8", value: "queenblack", color: "dark", piece: "Queen", pieceColor: "Black"}, 
+  {id: 5, row: 1, cell: 5, space: "e8", value: "kingblack", color: "light", piece: "King", pieceColor: "Black"}, 
+  {id: 6, row: 1, cell: 6, space: "f8", value: "bishopblack", color: "dark", piece: "Bishop", pieceColor: "Black"}, 
+  {id: 7, row: 1, cell: 7, space: "g8", value: "knightblack", color: "light", piece: "Knight", pieceColor: "Black"}, 
+  {id: 8, row: 1, cell: 8, space: "h8", value: "rookblack", color: "dark", piece: "Rook", pieceColor: "Black"},
 
-  {id: 9, row: 2, cell: 1, space: "a7", value: "", color: "dark", piece: "", pieceColor: ""}, {id: 10, row: 2, cell: 2, space: "b7", value: "", color: "light", piece: "", pieceColor: ""}, {id: 11, row: 2, cell: 3, space: "c7", value: "", color: "dark", piece: "", pieceColor: ""}, {id: 12, row: 2, cell: 4, space: "d7", value: "", color: "light", piece: "", pieceColor: ""}, {id: 13, row: 2, cell: 5, space: "e7", value: "", color: "dark", piece: "", pieceColor: ""}, {id: 14, row: 2, cell: 6, space: "f7", value: "", color: "light", piece: "", pieceColor: ""}, {id: 15, row: 2, cell: 7, space: "g7", value: "", color: "dark", piece: "", pieceColor: ""}, {id: 16, row: 2, cell: 8, space: "h7", value: "", color: "light", piece: "", pieceColor: ""},
+  {id: 9, row: 2, cell: 1, space: "a7", value: "pawnblack", color: "dark", piece: "Pawn", pieceColor: "Black"}, 
+  {id: 10, row: 2, cell: 2, space: "b7", value: "pawnblack", color: "light", piece: "Pawn", pieceColor: "Black"}, 
+  {id: 11, row: 2, cell: 3, space: "c7", value: "pawnblack", color: "dark", piece: "Pawn", pieceColor: "Black"}, 
+  {id: 12, row: 2, cell: 4, space: "d7", value: "pawnblack", color: "light", piece: "Pawn", pieceColor: "Black"}, 
+  {id: 13, row: 2, cell: 5, space: "e7", value: "pawnblack", color: "dark", piece: "Pawn", pieceColor: "Black"}, 
+  {id: 14, row: 2, cell: 6, space: "f7", value: "pawnblack", color: "light", piece: "Pawn", pieceColor: "Black"}, 
+  {id: 15, row: 2, cell: 7, space: "g7", value: "pawnblack", color: "dark", piece: "Pawn", pieceColor: "Black"}, 
+  {id: 16, row: 2, cell: 8, space: "h7", value: "pawnblack", color: "light", piece: "Pawn", pieceColor: "Black"},
 
   {id: 17, row: 3, cell: 1, space: "a6", value: "", color: "light", piece: "", pieceColor: ""}, {id: 18, row: 3, cell: 2, space: "b6", value: "", color: "dark", piece: "", pieceColor: ""}, {id: 19, row: 3, cell: 3, space: "c6", value: "", color: "light", piece: "", pieceColor: ""}, {id: 20, row: 3, cell: 4, space: "d6", value: "", color: "dark", piece: "", pieceColor: ""}, {id: 21, row: 3, cell: 5, space: "e6", value: "", color: "light", piece: "", pieceColor: ""}, {id:22, row: 3, cell: 6, space: "f6", value: "", color: "dark", piece: "", pieceColor: ""}, {id: 23, row: 3, cell: 7, space: "g6", value: "", color: "light", piece: "", pieceColor: ""}, {id: 24, row: 3, cell: 8, space: "h6", value: "", color: "dark", piece: "", pieceColor: ""},
 
@@ -13,9 +27,23 @@ const initialState = { board: [
 
   {id: 41, row: 6, cell: 1, space: "a3", value: "", color: "dark", piece: "", pieceColor: ""}, {id: 42, row: 6, cell: 2, space: "b3", value: "", color: "light", piece: "", pieceColor: ""}, {id: 43, row: 6, cell: 3, space: "c3", value: "", color: "dark", piece: "", pieceColor: ""}, {id: 44, row: 6, cell: 4, space: "d3", value: "", color: "light", piece: "", pieceColor: ""}, {id: 45, row: 6, cell: 5, space: "e3", value: "", color: "dark", piece: "", pieceColor: ""}, {id: 46, row: 6, cell: 6, space: "f3", value: "", color: "light", piece: "", pieceColor: ""}, {id: 47, row: 6, cell: 7, space: "g3", value: "", color: "dark", piece: "", pieceColor: ""}, {id: 48, row: 6, cell: 8, space: "h3", value: "", color: "light", piece: "", pieceColor: ""},
 
-  {id: 49, row: 7, cell: 1, space: "a2", value: "", color: "light", piece: "", pieceColor: ""}, {id: 50, row: 7, cell: 2, space: "b2", value: "", color: "dark", piece: "", pieceColor: ""}, {id: 51, row: 7, cell: 3, space: "c2", value: "", color: "light", piece: "", pieceColor: ""}, {id: 52, row: 7, cell: 4, space: "d2", value: "", color: "dark", piece: "", pieceColor: ""}, {id: 53, row: 7, cell: 5, space: "e2", value: "", color: "light", piece: "", pieceColor: ""}, {id: 54, row: 7, cell: 6, space: "f2", value: "", color: "dark", piece: "", pieceColor: ""}, {id: 55, row: 7, cell: 7, space: "g2", value: "", color: "light", piece: "", pieceColor: ""}, {id: 56, row: 7, cell: 8, space: "h2", value: "", color: "dark", piece: "", pieceColor: ""},
+  {id: 49, row: 7, cell: 1, space: "a2", value: "pawnwhite", color: "light", piece: "Pawn", pieceColor: "White"}, 
+  {id: 50, row: 7, cell: 2, space: "b2", value: "pawnwhite", color: "dark", piece: "Pawn", pieceColor: "White"}, 
+  {id: 51, row: 7, cell: 3, space: "c2", value: "pawnwhite", color: "light", piece: "Pawn", pieceColor: "White"}, 
+  {id: 52, row: 7, cell: 4, space: "d2", value: "pawnwhite", color: "dark", piece: "Pawn", pieceColor: "White"}, 
+  {id: 53, row: 7, cell: 5, space: "e2", value: "pawnwhite", color: "light", piece: "Pawn", pieceColor: "White"}, 
+  {id: 54, row: 7, cell: 6, space: "f2", value: "pawnwhite", color: "dark", piece: "Pawn", pieceColor: "White"}, 
+  {id: 55, row: 7, cell: 7, space: "g2", value: "pawnwhite", color: "light", piece: "Pawn", pieceColor: "White"}, 
+  {id: 56, row: 7, cell: 8, space: "h2", value: "pawnwhite", color: "dark", piece: "Pawn", pieceColor: "White"},
 
-  {id: 57, row: 8, cell: 1, space: "a1", value: "", color: "dark", piece: "", pieceColor: ""}, {id: 58, row: 8, cell: 2, space: "b1", value: "knightwhite", color: "light", piece: "Knight", pieceColor: "White"}, {id: 59, row: 8, cell: 3, space: "c1", value: "bishopwhite", color: "dark", piece: "Bishop", pieceColor: "White"}, {id: 60, row: 8, cell: 4, space: "d1", value: "", color: "light", piece: "", pieceColor: ""}, {id: 61, row: 8, cell: 5, space: "e1", value: "", color: "dark", piece: "", pieceColor: ""}, {id: 62, row: 8, cell: 6, space: "f1", value: "", color: "light", piece: "", pieceColor: ""}, {id: 63, row: 8, cell: 7, space: "g1", value: "", color: "dark", piece: "", pieceColor: ""}, {id: 64, row: 8, cell: 8, space: "h1", value: "", color: "light", piece: "", pieceColor: ""},
+  {id: 57, row: 8, cell: 1, space: "a1", value: "rookwhite", color: "dark", piece: "Rook", pieceColor: "White"}, 
+  {id: 58, row: 8, cell: 2, space: "b1", value: "knightwhite", color: "light", piece: "Knight", pieceColor: "White"}, 
+  {id: 59, row: 8, cell: 3, space: "c1", value: "bishopwhite", color: "dark", piece: "Bishop", pieceColor: "White"}, 
+  {id: 60, row: 8, cell: 4, space: "d1", value: "queenwhite", color: "light", piece: "Queen", pieceColor: "White"}, 
+  {id: 61, row: 8, cell: 5, space: "e1", value: "kingwhite", color: "dark", piece: "King", pieceColor: "White"}, 
+  {id: 62, row: 8, cell: 6, space: "f1", value: "bishopwhite", color: "light", piece: "Bishop", pieceColor: "White"}, 
+  {id: 63, row: 8, cell: 7, space: "g1", value: "knightwhite", color: "dark", piece: "Knight", pieceColor: "White"}, 
+  {id: 64, row: 8, cell: 8, space: "h1", value: "rookwhite", color: "light", piece: "Rook", pieceColor: "White"},
 ], turnColor: "White"}
 
 export default (state = initialState, action) => {
