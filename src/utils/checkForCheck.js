8d78cf8d11e2e64a4ffa601aed8fe06ef654401d
bigrocketgames <admin@bigrocketgames.com>
2018-05-13 07:53:03 -0700

    feat:  Started file to check for checks.

---
+++ src/utils/checkForCheck.js
@@ -0,0 +1,15 @@
+export const isKingChecked = () => {
+  //check if king is checked after another piece moves
+  //check only the king of the attacked color - if white moved, check on the black king and vice versa
+
+  //if king is checked, then call to check for checkmate
+}
+
+export const willKingBeChecked = () => {
+  //check if king will be checked if king moves to the new space
+}
+
+export const checkMate = () => {
+  // check this only if a king is checked - return true or false
+  // check this by testing for spots the king could perhaps move and if they are safe spaces
+}
