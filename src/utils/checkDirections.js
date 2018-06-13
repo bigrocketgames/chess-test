f3806dbdd91f085aee29d25a4ccd2b858fb688ac
bigrocketgames <admin@bigrocketgames.com>
2018-06-12 21:43:14 -0700

    chore:  Cleaning code.

--- src/utils/checkDirections.js
+++ src/utils/checkDirections.js
@@ -63,7 +63,7 @@ export const diagonalCheck = (updatedBoard, kingCell, attackColor, canCaptureWit
   // check to the top right - -vert, +hor
   const upRight = (movesUp < movesRight) ? movesUp : movesRight
   for (let i = 1; i <= upRight; i++) {
-    (i === 1 && canCaptureWithPawn && attackColor === "Black") ? checkingPieces = ["Bishop", "Queen", "Pawn"] : checkingPieces
+    if (i === 1 && canCaptureWithPawn && attackColor === "Black") checkingPieces = ["Bishop", "Queen", "Pawn"]
     const result = getCheckCell(updatedBoard, kingCell.row - i, kingCell.cell + i, checkingPieces, attackColor)
     if (result === "no threat") {
       break;
@@ -75,7 +75,7 @@ export const diagonalCheck = (updatedBoard, kingCell, attackColor, canCaptureWit
   // check to the top left - -vert, -hor
   const upLeft = (movesUp < movesLeft) ? movesUp : movesLeft
   for (let i = 1; i <= upLeft; i++) {
-    (i === 1 && canCaptureWithPawn && attackColor === "Black") ? checkingPieces = ["Bishop", "Queen", "Pawn"] : checkingPieces
+    if (i === 1 && canCaptureWithPawn && attackColor === "Black") checkingPieces = ["Bishop", "Queen", "Pawn"]
     const result = getCheckCell(updatedBoard, kingCell.row - i, kingCell.cell -i, checkingPieces, attackColor)
     if (result === "no threat") {
       break;
@@ -87,7 +87,7 @@ export const diagonalCheck = (updatedBoard, kingCell, attackColor, canCaptureWit
   // check to the bottom right - +vert, +hor
   const downRight = (movesDown < movesRight) ? movesDown : movesRight
     for (let i = 1; i <= downRight; i++) {
-      (i === 1 && canCaptureWithPawn && attackColor === "White") ? checkingPieces = ["Bishop", "Queen", "Pawn"] : checkingPieces
+      if (i === 1 && canCaptureWithPawn && attackColor === "White") checkingPieces = ["Bishop", "Queen", "Pawn"]
       const result = getCheckCell(updatedBoard, kingCell.row + i, kingCell.cell + i, checkingPieces, attackColor)
       if (result === "no threat") {
         break;
@@ -99,7 +99,7 @@ export const diagonalCheck = (updatedBoard, kingCell, attackColor, canCaptureWit
   // check to the bottom left - +vert, -hor
   const downLeft = (movesDown < movesLeft) ? movesDown : movesLeft
   for (let i = 1; i <= downLeft; i++) {
-    (i === 1 && canCaptureWithPawn && attackColor === "White") ? checkingPieces = ["Bishop", "Queen", "Pawn"] : checkingPieces
+    if (i === 1 && canCaptureWithPawn && attackColor === "White") checkingPieces = ["Bishop", "Queen", "Pawn"]
     const result = getCheckCell(updatedBoard, kingCell.row + i, kingCell.cell - i, checkingPieces, attackColor)
     if (result === "no threat") {
       break;
