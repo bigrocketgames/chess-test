f85cb4a6d652ad4909086e7b6b892561c65330c2
bigrocketgames <admin@bigrocketgames.com>
2018-06-21 20:33:10 -0700

    fix:  Fix breaking error due to wrong variable name.

--- src/utils/canBlockCheck.js
+++ src/utils/canBlockCheck.js
@@ -13,7 +13,7 @@ export const canBlockCheck = (board, turnColor, pieceToMove, cell) => {
   })
 
   // Return true if the piece that is checking the king can be captured on the next move by the team under attack
-  if (checkTheChecks(updatedBoard, testCell, underAttackColor, true, true)) {
+  if (checkTheChecks(updatedBoard, attackingPiece, underAttackColor, true, true)) {
     return true
   }
 
